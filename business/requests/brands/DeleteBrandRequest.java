511d9c224110c53e8806b593a1a3467a0f385eba
GoncaBal <goncabal94@gmail.com>
2022-06-09 20:52:53 +0300

    Initial commit

---
+++ business/requests/brands/DeleteBrandRequest.java
@@ -0,0 +1,12 @@
+package com.kodlamaio.rentACar.business.requests.brands;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeleteBrandRequest {
+	private int id;
+}
